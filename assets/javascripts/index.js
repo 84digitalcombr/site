8277f8aad69a72e7a92ebac5fef6fd4c4d365b1f
Luan Santos <luankjs@gmail.com>
2018-12-06 12:59:57 -0300

    Finalize portfolio section

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -9,3 +9,16 @@ $(document).on('click', '.topbar a[href^="#"]', function (event) {
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 1000);
 });
+
+$(document).on('show.bs.modal', '#modal-project', function (event) {
+  var project = $(event.relatedTarget);
+  var title = project.data('title');
+  var description = project.data('description');
+  var client = project.data('client');
+  var image_path = project.data('image-path');
+
+  var modal = $(this);
+  modal.find('.project-title').text(title);
+  modal.find('.project-description').text(description);
+  modal.find('.project-image').attr('src', image_path);
+})
